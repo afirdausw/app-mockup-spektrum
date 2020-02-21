a0e8b19d8ad78dde710d1809eb2c4532c0f86c2f
Afif Firdaus W <afiefpeace@gmail.com>
2020-02-21 22:33:58 +0700

    upload repository

---
+++ app/src/main/java/mockup/spektrum/Signin.java
@@ -0,0 +1,14 @@
+package mockup.spektrum;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signin extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_signin);
+    }
+}
