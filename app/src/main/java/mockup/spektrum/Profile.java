8916340e125a5b14197bd6fa7c7ac4e6e0e06c99
Afif Firdaus W <afiefpeace@gmail.com>
2020-02-24 21:47:45 +0700

    Finishing profile

--- app/src/main/java/mockup/spektrum/Profile.java
+++ app/src/main/java/mockup/spektrum/Profile.java
@@ -3,10 +3,13 @@ package mockup.spektrum;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
+
+    TextView name, gender, date, address, phone, username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,31 @@ public class Profile extends AppCompatActivity {
 
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        name = findViewById(R.id.name);
+        gender = findViewById(R.id.gender);
+        date = findViewById(R.id.date);
+        address = findViewById(R.id.address);
+        phone = findViewById(R.id.phone);
+        username = findViewById(R.id.username);
+
+        name.setText("Afif Firdaus Wipayana");
+        gender.setText("Male");
+        date.setText("09-09-1999");
+        address.setText("Jl. Kenanga Indah 1 No.12D, Jatimulyo, Lowokwaru, Malang");
+        phone.setText("0812-4040-0042");
+        username.setText("Firwyna");
     }
 
     public void onNotification(View view) {
+
+    }
+
+    public void onEdit(View view) {
+
+    }
+
+    public void onLogout(View view) {
 
     }
 }
